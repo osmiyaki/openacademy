0b716b75beaea72122610194a2be5c91e3976a66
OscarSM <oscar.sabaleta@jarsa.com.mx>
2015-04-13 12:47:44 -0500

    [ADD]: Added Exercise: Calendar view in models.py & openacademy.xml

--- models/models.py
+++ models/models.py
@@ -1,5 +1,6 @@
 # -*- coding: utf-8 -*-
 
+from datetime import timedelta
 from openerp import models, fields, api, exceptions
 
 class course(models.Model):
@@ -54,6 +55,8 @@ class Session(models.Model):
 	attendee_ids = fields.Many2many('res.partner', string="Attendees")
 
 	taken_seats = fields.Float(string="Taken seats", compute='_taken_seats')
+	end_date = fields.Date(string="End Date", store=True,
+		compute='_get_end_date', inverse='_set_end_date')
 
 	@api.one
 	@api.depends('seats', 'attendee_ids')
@@ -79,6 +82,29 @@ class Session(models.Model):
 					'message': "Increase seats or remove excess attendees",
 				},
 			}
+	@api.one
+	@api.depends('start_date', 'duration')
+	def _get_end_date(self):
+		if not (self.start_date and self.duration):
+			return
+
+		# Add duration to start_date, but: Monday + 5 days = Saturday, so
+		# subtract one second to get friday instead
+		start = fields.Datetime.from_string(self.start_date)
+		duration = timedelta(days=self.duration, seconds=-1)
+		self.end_date = start + duration
+
+	@api.one
+	def _set_end_date(self):
+		if not (self.start_date and self.end_date):
+			return
+
+		# Compute the difference between dates, but friday - monday = 4 days,
+		# so add one day to get 5 days instead
+		start_date = fields.Datetime.from_string(self.start_date)
+		end_date = fields.Datetime.from_string(self.end_date)
+		self.duration = (end_date - start_date).days + 1
+
 	@api.one
 	@api.constrains('instructor_id', 'attendee_ids')
 	def _check_instructor_not_in_attendees(self):
